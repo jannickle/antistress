95c71e2595107b9d6793ebd5555b9dcbde2ac4aa
Peter <iw4ffl3z@gmail.com>
2020-11-25 00:19:53 +0100

    Initial commit

---
+++ src/main/java/com/example/antistressdagbog/AntistressdagbogApplication.java
@@ -0,0 +1,13 @@
+package com.example.antistressdagbog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntistressdagbogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntistressdagbogApplication.class, args);
+    }
+
+}
