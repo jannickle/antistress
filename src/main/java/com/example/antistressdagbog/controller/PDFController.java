ff1f4e09c56021d6e739ba4102a6c4f3d6b61199
Peter <iw4ffl3z@gmail.com>
2020-12-13 00:59:37 +0100

    Footer template implemented About page implemented with pdf iframe

--- src/main/java/com/example/antistressdagbog/controller/PDFController.java
+++ src/main/java/com/example/antistressdagbog/controller/PDFController.java
@@ -2,42 +2,39 @@ package com.example.antistressdagbog.controller;
 
 import com.example.antistressdagbog.utility.FileHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Controller
 public class PDFController {
 
     @GetMapping("/guide/view")
-    public String view(){
+    public String viewGuide(){
         return "guide/view";
     }
-    @GetMapping("/navbar")
-    public String viewnav(){
 
-        return "navbar";
-    }
-
-    @GetMapping("/velkommen")
-    public String velkommen(){
-
-        return "velkommen";
+    @GetMapping("/guide/about")
+    public String viewAbout(){
+        return "guide/about";
     }
 
     @RequestMapping("/howto")
-    public void getFile(HttpServletResponse response) throws IOException {
+    public void getGuide(HttpServletResponse response) throws IOException {
         FileHandler fileHandler = new FileHandler();
         fileHandler.readfile("sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf","src/main/resources/static/files/",response);
 //        String fileName = "sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf";
 //        String path = "src/main/resources/static/files/" + fileName;
+    }
 
-
+    @RequestMapping("/about")
+    public void getAbout(HttpServletResponse response) throws IOException {
+        FileHandler fileHandler = new FileHandler();
+        fileHandler.readfile("hvad-er-antistress-dagbogenthauer-stresscenter.pdf","src/main/resources/static/files/",response);
+//        String fileName = "sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf";
+//        String path = "src/main/resources/static/files/" + fileName;
     }
 
 }
