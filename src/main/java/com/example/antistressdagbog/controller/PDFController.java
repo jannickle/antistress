697d458d37ad81c597849021fd0773dfc83bb9eb
Peter <iw4ffl3z@gmail.com>
2020-12-13 23:13:10 +0100

    Attemp to fix PDF's not working on production

--- src/main/java/com/example/antistressdagbog/controller/PDFController.java
+++ src/main/java/com/example/antistressdagbog/controller/PDFController.java
@@ -21,13 +21,13 @@ public class PDFController {
         return "guide/about";
     }
 
-    @RequestMapping("/howto")
-    public void getGuide(HttpServletResponse response) throws IOException {
-        FileHandler fileHandler = new FileHandler();
-        fileHandler.readfile("sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf","src/main/resources/static/files/",response);
-//        String fileName = "sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf";
-//        String path = "src/main/resources/static/files/" + fileName;
-    }
+//    @RequestMapping("/howto")
+//    public void getGuide(HttpServletResponse response) throws IOException {
+//        FileHandler fileHandler = new FileHandler();
+//        fileHandler.readfile("sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf","src/main/resources/static/files/",response);
+////        String fileName = "sdan-udfylder-du-antistress-dagbogenthauer-stresscenter.pdf";
+////        String path = "src/main/resources/static/files/" + fileName;
+//    }
 //
 //    @RequestMapping("/about")
 //    public void getAbout(HttpServletResponse response) throws IOException {
